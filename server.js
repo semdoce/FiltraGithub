64919dde28b23f08fcd45fae8776634b4c553360
semdoce <g.andrade2431@outlook.com>
2026-09-11 11:27:30 -0300

    feat: add favicon route to prevent 404 CSP errors

--- server.js
+++ server.js
@@ -13,6 +13,9 @@ app.get('/', (req, res) => {
     res.render('index', { commits: null, summary: null, error: null });
 });
 
+// Ignorar requisições de favicon para evitar erros de CSP no 404 do Express
+app.get('/favicon.ico', (req, res) => res.status(204).end());
+
 // Rota que chama o Controller
 app.get('/search', CommitController.checkUserCommits);
 
